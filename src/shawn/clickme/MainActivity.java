04ef8e17daa8471960fc879b24588d142959094a
default_git_name <default_git_email>
2016-12-09 21:00:40 +0000

    first commit

---
+++ src/shawn/clickme/MainActivity.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shawn.clickme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ *
+ * @author Spartacus Rex
+ */
+public class MainActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        // ToDo add your GUI initialization code here
+        setContentView(R.layout.main);
+	
+
+    }
+
+}
